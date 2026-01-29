12aebfd205beca396be0b503114fbc2da00b266c
Jonathan-dev2002 <jonathandoillon2002@gmail.com>
2026-01-29 10:34:49 +0700

    first commit

---
+++ learning-react-ts/state/src/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+
+function App() {
+  const [name, setName] = useState<string>('jonathan')
+  const [salary, setSalary] = useState<number>(20000)
+  const [isVisiable, setIsvisble] = useState<boolean>(true)
+  return (
+    <>
+      {isVisiable &&
+        (<div>
+          <h1>พนักงาน : {name}</h1>
+          <h2>เงินเดือน : {salary} บาท</h2>
+        </div>)
+      }
+
+      <hr />
+      <input value={name} onChange={(e) => setName(e.target.value)} />
+      <button onClick={() => setSalary(salary + 1000)}>+</button>
+      <button onClick={() => setSalary(salary - 1000)}>-</button>
+      <button onClick={()=> setIsvisble(!isVisiable)}>{isVisiable ? "ซ่อน" : "แสดง"}</button>
+    </>
+  )
+}
+
+export default App
